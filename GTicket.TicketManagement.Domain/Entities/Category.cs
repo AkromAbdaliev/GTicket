9f98e9c5d1181668c49ddea6cea551ef1fe71d5f
AkromAbdaliev <jacquesalaiyxs@gmail.com>
2024-10-03 12:11:13 +0300

    Add project files.

---
+++ GTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using GTicket.TicketManagement.Domain.Common;
+
+namespace GTicket.TicketManagement.Domain.Entities
+{
+    public class Category: AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
